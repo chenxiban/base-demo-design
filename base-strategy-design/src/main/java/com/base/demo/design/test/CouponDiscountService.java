69a78fe85cc6570521506de87a65b5fe69e88795
Sunny Chen <chen867647213@163.com>
2021-07-03 10:44:31 +0800

    提交内容：提交策略设计模式--基础案例优化；提交人：Sunny Chen；提交时间：2021-7-3 10:44:20

--- base-strategy-design/src/main/java/com/base/demo/design/test/CouponDiscountService.java
+++ base-strategy-design/src/main/java/com/base/demo/design/test/CouponDiscountService.java
@@ -20,7 +20,13 @@ import com.base.demo.design.utils.model.enums.SelectTypeEnum;
 public class CouponDiscountService {
 
     /**
-     * 优惠券折扣计算接口
+     * 优惠券折扣计算接口--这里也可以结合其他设计模式干掉if-else
+     * <p>
+     * 以下是不同类型的优惠券计算折扣后的实际金额。
+     * <p>
+     * 入参包括；优惠券类型、优惠券金额、商品金额，因为有些优惠券是满多少减少多少，所以增加了typeExt类型。这也是方法的不好扩展性问题。
+     * <p>
+     * 最后是整个的方法体中对优惠券抵扣金额的实现，最开始可能是一个最简单的优惠券，后面随着产品功能的增加，不断的扩展if语句。实际的代码可能要比这个多很多。
      *
      * @param type        券类型
      * @param typeContent 券内容
--- base-strategy-design/src/main/java/com/base/demo/design/test/context/Context.java
+++ base-strategy-design/src/main/java/com/base/demo/design/test/context/Context.java
@@ -5,7 +5,7 @@ import com.base.demo.design.test.ICouponDiscount;
 import java.math.BigDecimal;
 
 /**
- * @Description: 上下文
+ * @Description: 策略控制类上下文
  * @BelongsProject: base-demo-design
  * @BelongsPackage: com.base.demo.design.service.context
  * @Author: ChenYongJia
@@ -15,6 +15,11 @@ import java.math.BigDecimal;
  */
 public class Context<T> {
 
+    /**
+     * 策略模式的控制类主要是外部可以传递不同的策略实现，在通过统一的方法执行优惠策略计算。
+     * <p>
+     * 另外这里也可以包装成 map 结构，让外部只需要对应的泛型类型即可使用相应的服务。
+     */
     private ICouponDiscount<T> couponDiscount;
 
     public Context(ICouponDiscount<T> couponDiscount) {
@@ -22,7 +27,7 @@ public class Context<T> {
     }
 
     /**
-     * 折算后的价格
+     * 折算后的价格--泛型的使用可以传递不同的类型参数。
      *
      * @param couponInfo 优惠券信息
      * @param skuPrice   sku价格

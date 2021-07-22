013c38f056a6002295b3f65d5091cedaf41debc3
Sunny Chen <chen867647213@163.com>
2021-07-22 14:23:26 +0800

    提交内容：1.提交桥接设计模式，基础案例代码；2.整理 README.md 帮助阅读文件；提交人：Sunny Chen；提交时间：2021-7-13 14:44:30

--- base-strategy-design/src/test/java/com/base/demo/design/test/ApiTest.java
+++ base-strategy-design/src/test/java/com/base/demo/design/test/ApiTest.java
@@ -35,7 +35,7 @@ public class ApiTest {
     @Test
     public void test_zj() {
         // 直减；100-10，商品100元
-        Context<Double> context = new Context<Double>(new ZJCouponDiscount());
+        Context<Double> context = new Context<>(new ZJCouponDiscount());
         BigDecimal discountAmount = context.discountAmount(10D, new BigDecimal(100));
         log.info("测试结果：直减优惠后金额 {}", discountAmount);
     }
@@ -51,8 +51,8 @@ public class ApiTest {
     @Test
     public void test_mj() {
         // 满100减10，商品100元
-        Context<Map<String, String>> context = new Context<Map<String, String>>(new MJCouponDiscount());
-        Map<String, String> mapReq = new HashMap<String, String>();
+        Context<Map<String, String>> context = new Context<>(new MJCouponDiscount());
+        Map<String, String> mapReq = new HashMap<>();
         mapReq.put("x", "100");
         mapReq.put("n", "10");
         BigDecimal discountAmount = context.discountAmount(mapReq, new BigDecimal(100));
@@ -70,7 +70,7 @@ public class ApiTest {
     @Test
     public void test_zk() {
         // 折扣9折，商品100元
-        Context<Double> context = new Context<Double>(new ZKCouponDiscount());
+        Context<Double> context = new Context<>(new ZKCouponDiscount());
         BigDecimal discountAmount = context.discountAmount(0.9D, new BigDecimal(100));
         log.info("测试结果：折扣9折后金额 {}", discountAmount);
     }
@@ -86,7 +86,7 @@ public class ApiTest {
     @Test
     public void test_nyg() {
         // n元购；100-10，商品100元
-        Context<Double> context = new Context<Double>(new NYGCouponDiscount());
+        Context<Double> context = new Context<>(new NYGCouponDiscount());
         BigDecimal discountAmount = context.discountAmount(90D, new BigDecimal(100));
         log.info("测试结果：n元购优惠后金额 {}", discountAmount);
     }
